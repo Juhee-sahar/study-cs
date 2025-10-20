f8fd328fad66630a062b8f0efa4f4a01761b9327
juhee <jh7895214@gmail.com>
2025-10-20 22:58:10 +0900

    pong game init

---
+++ PongGame/PongGame/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PongGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            player = new PictureBox();
+            computer = new PictureBox();
+            ball = new PictureBox();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)player).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)computer).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ball).BeginInit();
+            SuspendLayout();
+            // 
+            // player
+            // 
+            player.Image = Properties.Resources.player;
+            player.Location = new Point(12, 191);
+            player.Name = "player";
+            player.Size = new Size(30, 120);
+            player.SizeMode = PictureBoxSizeMode.StretchImage;
+            player.TabIndex = 0;
+            player.TabStop = false;
+            // 
+            // computer
+            // 
+            computer.Image = Properties.Resources.computer;
+            computer.Location = new Point(940, 191);
+            computer.Name = "computer";
+            computer.Size = new Size(30, 120);
+            computer.SizeMode = PictureBoxSizeMode.StretchImage;
+            computer.TabIndex = 1;
+            computer.TabStop = false;
+            // 
+            // ball
+            // 
+            ball.Image = Properties.Resources.ball;
+            ball.Location = new Point(502, 240);
+            ball.Name = "ball";
+            ball.Size = new Size(30, 30);
+            ball.SizeMode = PictureBoxSizeMode.StretchImage;
+            ball.TabIndex = 2;
+            ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 20;
+            GameTimer.Tick += GameTimeEvent;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(982, 553);
+            Controls.Add(ball);
+            Controls.Add(computer);
+            Controls.Add(player);
+            DoubleBuffered = true;
+            Name = "Form1";
+            Text = "Player : 0 -- Computer : 0";
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)player).EndInit();
+            ((System.ComponentModel.ISupportInitialize)computer).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ball).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox player;
+        private PictureBox computer;
+        private PictureBox ball;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
---
+++ PongGame/README.md
@@ -0,0 +1,12 @@
+![화면이미지](PongGame/Resources/screen01.png)
+
+# 출처
+**youtube - Moo ICT**  <br>
+https://www.youtube.com/watch?v=d3SMWDPGQyw&list=PLqOxH0kcZ8wPB5ydzHw81hGDdO85b-JsR&index=53
+
+# DoubleBuffered
+화면 깜빡임을 줄이기 위해 사용하는 그래픽 렌더링 기술과 관련
+
+**DoubleBuffered = True**
+화면 깜빡임이 줄어듭니다.
+
