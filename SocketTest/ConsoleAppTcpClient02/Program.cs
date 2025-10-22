81f9e23cbd3d63214e74e419d6e2661b4c65c69a
juhee <jh7895214@gmail.com>
2025-10-22 10:37:42 +0900

    Tcp Socket Client 콘솔 예제

--- SocketTest/ConsoleAppTcpClient02/Program.cs
+++ SocketTest/ConsoleAppTcpClient02/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace ConsoleAppTcpClient02
+﻿using TCPSocketAsync;
+
+namespace ConsoleAppTcpClient02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TCPSocketClient client = new TCPSocketClient();
+
+            Console.WriteLine("*** 소켓 클라이언트 예제 ***");
+
+            Console.WriteLine("서버 IP 입력 : ");
+            string strIPAddress = (Console.ReadLine() ?? string.Empty).Trim();
+
+            Console.WriteLine("포트 번호(1~65535)를 입력하세요 : ");
+            string strPortInput = (Console.ReadLine() ?? string.Empty).Trim();  
+
+            // IP 나 Port 가 잘못된 경우
+            if (!client.SetServerIPAddress(strIPAddress) || !client.SetServerPort(strPortInput))
+            {
+                Console.WriteLine($" 잘못된 IP 또는 Port. IP : {strIPAddress}, Port : {strPortInput}");
+                Console.WriteLine("아무 키나 누르면 종료합니다.");
+                Console.ReadKey();
+                return;
+            }
+
+            // 서버에 비동기 접속 시도
+            _ = client.ConnectToServerAsync();
+
+            // 사용자 입력 대기
+            string? userInput = null;
+            Console.WriteLine("메시지를 입력하세요. (종료하려면 <EXIT> 입력 후 Enter");
+
+            do
+            {
+                userInput = Console.ReadLine() ?? string.Empty;
+            } while (userInput != "<EXIT>");
+
+            Console.WriteLine("클라이언트 프로그램을 종료합니다.");
         }
     }
 }
--- SocketTest/TCPSocketAsync/TCPSocketClient.cs
+++ SocketTest/TCPSocketAsync/TCPSocketClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,60 @@ namespace TCPSocketAsync
 {
     public class TCPSocketClient
     {
+        // 서버 접속에 필요한 데이터 준비
+        private IPAddress? _serverIPAddress = IPAddress.Parse("127.0.0.1");
+        private int _serverPort;
+        private TcpClient? _client;
+
+        // getter 구현 (외부에서 변경 불가, 내부 메서드를 통해서만 변경)
+        public IPAddress? ServerIPAddress => _serverIPAddress;
+        public int ServerPort => _serverPort;
+        public TcpClient? Client => _client;
+
+        // setter 구현 (IP 검증 및 설정)
+        public bool SetServerIPAddress(string addressStr)
+        {
+            if (!IPAddress.TryParse(addressStr, out var parsed))
+            {
+                Console.WriteLine(" 잘못된 서버 IP입니다.");
+                return false;   
+            }
+
+            _serverIPAddress = parsed;
+            return true;
+        }
+
+        // setter 구현 (포트 번호 검증 및 설정 메서드)
+        public bool SetServerPort(string portStr)
+        {
+            int portNum;
+            if (!int.TryParse(portStr, out portNum) || portNum <= 0 || portNum > 65535)
+            {
+                Console.WriteLine(" 잘못된 Port입니다. (유효 범위 : 1~65535)");
+                return false;   
+            }
+
+            _serverPort = portNum;  
+            return true;    
+        }
+
+        // 서버 접속 메서드
+        public async Task ConnectToServerAsync()
+        {
+            // 서버가 연결되어 있지 않으면
+            if (_client == null) _client = new TcpClient();
+
+            try
+            {
+                await _client.ConnectAsync(_serverIPAddress, _serverPort);
+                Console.WriteLine(string.Format("서버 접속 IP/Port : {0}/{1}", _serverIPAddress, _serverPort));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"서버 접속 에러 : {e.ToString()}");
+                throw;
+            }
+        }
     }
 }
